68194057a02fa10abd59e26f46f9c6f04ace8dd4
benguakbostanci <bengu.akbostanci@ug.bilkent.edu.tr>
2015-12-09 14:20:39 +0200

    Update HighScoreScreen.java

--- CS319_Project/src/userinterface/HighScoreScreen.java
+++ CS319_Project/src/userinterface/HighScoreScreen.java
@@ -1,16 +1,42 @@
 package userinterface;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
-import gamecontrol.GameManager;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class HighScoreScreen extends GameScreen implements Observer
 {
 
-	public HighScoreScreen( ScreenManager screenManager, GameManager gameManager)
+	public HighScoreScreen( ScreenManager screenManager)
 	{
-		super( screenManager, gameManager);
+		super( screenManager);
+		gameManager.setHighScoreScreen(this);
+		
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("High Scores");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 28));
+		lblNewLabel.setForeground(new Color(255, 0, 0));
+		lblNewLabel.setBounds(292, 55, 293, 94);
+		add(lblNewLabel);
+		
+		JButton btnGoToMain = new JButton("Go to Main Menu");
+		btnGoToMain.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==btnGoToMain){
+					screenManager.openScreen("main menu");
+				}
+			}
+		});
+		btnGoToMain.setBounds(29, 510, 140, 38);
+		add(btnGoToMain);
 	}
 
 	@Override
