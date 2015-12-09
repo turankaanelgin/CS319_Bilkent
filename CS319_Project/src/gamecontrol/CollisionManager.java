924fe5c72776e86e51dbf6d0c7748a95b6050724
OnurUygur <onur.uygur@ug.bilkent.edu.tr>
2015-12-10 00:37:47 +0200

    Update CollisionManager.java

--- CS319_Project/src/gamecontrol/CollisionManager.java
+++ CS319_Project/src/gamecontrol/CollisionManager.java
@@ -55,13 +55,28 @@ public class CollisionManager
 					
 				for (int i = 0; i < indices.size(); i++)
 					sequence.removeBall( indices.get( i));
-				index = indices.get( indices.size() - 1);	
+				index = indices.get( indices.size() - 1);
+				levelManager.playSound(Ball.BallType.PLAIN);
 			}
 			else
 				break;
 			
 			levelManager.getLevelScreen().repaint();
 		}
+		if(hit && current.getType() == Ball.BallType.BACK)
+		{
+			levelManager.rewind();
+			levelManager.playSound(Ball.BallType.BACK);
+			hit = false;
+		}
+		
+		else if(hit && current.getType() == Ball.BallType.BOMB)
+		{
+			levelManager.explode();
+			levelManager.playSound(Ball.BallType.BOMB);
+			hit = false;
+		}
+		levelManager.getLevelScreen().repaint();
 		levelManager.startSequence();
 	}
 }
