16decdd4a6c6cefa698ca5d6caee24539fd9487b
OnurUygur <onur.uygur@ug.bilkent.edu.tr>
2015-12-10 00:35:25 +0200

    Update SoundManager.java

--- CS319_Project/src/gamecontrol/SoundManager.java
+++ CS319_Project/src/gamecontrol/SoundManager.java
@@ -1,8 +1,13 @@
 package gamecontrol;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.event.*;
+
+import gameentity.Ball;
 import gameentity.Ball.BallType;
 
-public class SoundManager
+public class SoundManager extends Applet
 {
 	private LevelManager levelManager;
 	private final int HIT_SOUND = 0;
@@ -10,6 +15,9 @@ public class SoundManager
 	private final int BACK_SOUND = 0;
 	private final int FREEZE_SOUND = 0;
 	private final int COIN_SOUND = 0;
+	private boolean isOn;
+	
+	private AudioClip HIT, FREEZE, BOMB, BACK;
 	
 	public SoundManager( LevelManager levelManager)
 	{
@@ -18,6 +26,29 @@ public class SoundManager
 	
 	public void playSound( BallType type)
 	{
-		// todo
+		if(type == Ball.BallType.PLAIN)
+		{
+			HIT = getAudioClip(getCodeBase(), "game_sounds/HIT.wav");
+			HIT.play();
+		}
+		
+		else if(type == Ball.BallType.FREEZE)
+		{
+			FREEZE = getAudioClip(getCodeBase(), "game_sounds/FREEZE.wav");
+			FREEZE.play();
+		}
+		
+		else if(type == Ball.BallType.BOMB)
+		{
+			BOMB = getAudioClip(getCodeBase(), "game_sounds/BOMB.wav");
+			BOMB.play();
+		}
+		
+		else if(type == Ball.BallType.BACK)
+		{
+			BACK = getAudioClip(getCodeBase(), "game_sounds/BACK.wav");
+			BACK.play();
+		}
 	}
 }
+
