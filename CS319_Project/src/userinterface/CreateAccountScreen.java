81556798bf7ecd374b18350752bdf8d0021b37ff
benguakbostanci <bengu.akbostanci@ug.bilkent.edu.tr>
2015-12-09 14:02:29 +0200

    Update CreateAccountScreen.java

--- CS319_Project/src/userinterface/CreateAccountScreen.java
+++ CS319_Project/src/userinterface/CreateAccountScreen.java
@@ -1,67 +1,106 @@
+
 package userinterface;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-import gamecontrol.GameManager;
-
-public class CreateAccountScreen extends GameScreen 
+public class CreateAccountScreen extends GameScreen
 {
 	private static final long serialVersionUID = 1L;
 	private ShortcutListener shortcutListener;
 	private JLabel nameLabel;
-	private JTextField nameField;
-	private JPasswordField passField;
-	private JButton confirm, back;
 	
-	public CreateAccountScreen( ScreenManager screenManager, GameManager gameManager)
+	public CreateAccountScreen( ScreenManager screenManager)
 	{
-		super( screenManager, gameManager);
+		super( screenManager);
 		gameManager.setCreateAccountScreen( this);
 		
-		nameLabel = new JLabel( "Enter username: ");
+		/*nameLabel = new JLabel( "Enter username: ");
 		nameLabel.setBounds( 200, 200, 150, 30);
 		nameLabel.setFont( new Font( "Times", Font.BOLD, 16));
+		add( nameLabel);*/
 		
-		nameField = new JTextField( 10);
-		nameField.setBounds( 400, 200, 100, 30);
-		passField = new JPasswordField( 10);
-		passField.setBounds( 400, 270, 100, 30);
 		
-		confirm = new JButton( "Confirm");
-		confirm.setBounds( 400, 330, 100, 30);
-		confirm.addActionListener( this);
-		back = new JButton( "<");
-		back.setBounds( 50, 50, 50, 50);
-		back.addActionListener( this);
+		//setBounds(150, 150, 780, 700);
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
 		
-		add( nameLabel);
-		add( nameField);
-		add( passField);
-		add( confirm);
-		add( back);
+		TextField textField = new TextField();
+		textField.setBounds(287, 141, 170, 22);
+		add(textField);
+		
+		TextField textField_1 = new TextField();
+		textField_1.setBounds(287, 200, 170, 22);
+		add(textField_1);
+		
+		TextField textField_2 = new TextField();
+		textField_2.setBounds(287, 256, 170, 22);
+		add(textField_2);
+		
+		JLabel lblNewLabel = new JLabel("Enter your user name here");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(new Color(0, 0, 255));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel.setBounds(270, 121, 211, 14);
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Enter your password here");
+		lblNewLabel_1.setForeground(new Color(0, 0, 255));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(287, 180, 187, 14);
+		add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Enter your password again");
+		lblNewLabel_2.setForeground(new Color(0, 0, 255));
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_2.setBounds(287, 236, 187, 14);
+		add(lblNewLabel_2);
+
+		
+		JButton btnNewButton = new JButton("Create new account");
+		btnNewButton.setBounds(305, 324, 129, 23);
+		add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFrame invalidPlayerFrame= new JFrame();
+				invalidPlayerFrame.setBounds(100, 100, 400, 200);
+				invalidPlayerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				invalidPlayerFrame.getContentPane().setLayout(null);
+				
+				JLabel invalidPlayerLabel = new JLabel("Invalid Player");
+				invalidPlayerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+				invalidPlayerLabel.setForeground(new Color(255, 0, 0));
+				invalidPlayerLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+				invalidPlayerLabel.setBounds(100, 50, 220, 50);
+				invalidPlayerFrame.getContentPane().add(invalidPlayerLabel);
+				
+				invalidPlayerFrame.setVisible(true);
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("Go to login page");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==btnNewButton_1){
+					screenManager.openScreen("login");
+				}
+			}
+		});
+		btnNewButton_1.setBounds(30, 509, 157, 31);
+		add(btnNewButton_1);
+		
 	}
 	
-	@SuppressWarnings("deprecation")
-	@Override
-	public void actionPerformed( ActionEvent e)
+	public void returnToLoginScreen()
 	{
-		if (e.getSource() == confirm)
-		{
-			String name, password;
-			
-			name = nameField.getText();
-			password = passField.getText();
-			
-			gameManager.addPlayer( name, password);
-		}
-		else if (e.getSource() == back)
-			screenManager.openScreen( "login");
+		
 	}
 }
